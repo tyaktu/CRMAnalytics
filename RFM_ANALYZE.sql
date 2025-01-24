c0a32a9a1788c1129098b590734ea0bb16ea1733
tyaktu <tyaktu@users.noreply.github.com>
2025-01-24 15:44:06 +0300

    Add files via upload

---
+++ RFM_ANALYZE.sql
@@ -0,0 +1,76 @@
+/*
+Tahsin Bekir YAKTU 
+https://github.com/tyaktu/CRMAnalytics
+https://medium.com/@tahsinyaktu
+www.linkedin.com/in/tby1
+*/
+
+
+USE [RFM]
+IF OBJECT_ID('[dbo].[RFM2012]','U') IS NOT NULL
+	DROP TABLE [dbo].[RFM2012]
+
+	SET ANSI_NULLS ON
+	SET QUOTED_IDENTIFIER ON
+
+	CREATE TABLE [dbo].[RFM2012](
+		[CustomerID] [varchar](20) NULL,
+		[LastInvoiceDate] [date] NULL,
+		[Recency] [int] NULL,
+		[Frequency] [int] NULL,
+		[Monatery] [money] NULL,
+		[Recency_Scale] [int] NULL,
+		[Frequency_Scale] [int] NULL,
+		[Monatery_Scale] [int] NULL,
+		[Score] [int] NULL,
+		[Segment] [varchar](100) NULL,
+		[Segment2] [varchar](100) NULL
+	) ON [PRIMARY]
+
+	SET ANSI_PADDING ON
+
+		CREATE UNIQUE CLUSTERED INDEX [ClusteredIndex-CustomerID] ON [dbo].[RFM2012]
+		(
+			[CustomerID] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+		GO
+
+
+TRUNCATE TABLE RFM2012
+GO
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+INSERT INTO RFM2012(CustomerID) SELECT DISTINCT [CUSTOMER ID] FROM ONLINERETAIL_2012
+GO
+UPDATE RFM2012 SET LastInvoiceDate =t.mInvoiceDate  from RFM2012 r inner join (select  [Customer ID],MAX(InvoiceDate)mInvoiceDate from ONLINERETAIL_2012 GROUP BY [Customer ID]) t on r.customerID=t.[Customer ID]
+GO
+UPDATE RFM2012 SET Recency=DATEDIFF(DAY,LastInvoiceDate,'2013-01-01')
+GO
+UPDATE RFM2012 SET Frequency=t.cInvoice from RFM2012 r inner join (select  [Customer ID],COUNT(Distinct Invoice) cInvoice from ONLINERETAIL_2012 GROUP BY [Customer ID]) t on r.customerID=t.[Customer ID]
+GO
+UPDATE RFM2012 SET Monatery=t.sMonatary from RFM2012 r inner join (SELECT [Customer ID],sum([Price]*[Quantity])  sMonatary from ONLINERETAIL_2012 GROUP BY [Customer ID]) t on r.customerID=t.[Customer ID]
+GO
+update RFM2012 SET Recency_Scale=( select RANK from ( SELECT  *, NTILE(5) OVER( ORDER BY Recency desc) Rank FROM RFM2012 ) t where  CustomerID=RFM2012.CustomerID)
+GO
+update RFM2012 SET Frequency_Scale=( select RANK from ( SELECT  *, NTILE(5) OVER( ORDER BY Frequency) Rank FROM RFM2012 ) t where  CustomerID=RFM2012.CustomerID)
+GO
+update RFM2012 SET Monatery_Scale=0
+update RFM2012 SET Monatery_Scale=( select RANK from ( SELECT  *, NTILE(5) OVER( ORDER BY Monatery) Rank FROM RFM2012 where Monatery>'5' ) t where  CustomerID=RFM2012.CustomerID)
+GO
+UPDATE RFM2012 SET Score=0
+UPDATE RFM2012 SET Score=cast(recency_scale as nvarchar(1))+cast(Frequency_Scale as nvarchar(1))+cast(Monatery_Scale as nvarchar(1))  FROM [RFM].[dbo].[RFM2012] 
+GO
+UPDATE RFM2012 SET Segment ='Hibernating Customers'			WHERE Recency_Scale LIKE  '[1-2]%' AND Frequency_Scale LIKE '[1-2]%' --
+UPDATE RFM2012 SET Segment ='At Risk'						WHERE Recency_Scale LIKE  '[1-2]%' AND Frequency_Scale LIKE '[3-4]%' --
+UPDATE RFM2012 SET Segment ='Cannot Lose Them But Losing'	WHERE Recency_Scale LIKE  '[1-2]%' AND Frequency_Scale LIKE '[5]%'   
+UPDATE RFM2012 SET Segment ='About To Sleep'				WHERE Recency_Scale LIKE  '[3]%' AND Frequency_Scale LIKE '[1-2]%'   --
+UPDATE RFM2012 SET Segment ='Need Attention'				WHERE Recency_Scale LIKE  '[3]%' AND Frequency_Scale LIKE '[3]%' 	 
+UPDATE RFM2012 SET Segment ='Loyal'							WHERE Recency_Scale LIKE  '[3-4]%' AND Frequency_Scale LIKE '[4-5]%' 
+UPDATE RFM2012 SET Segment ='Promising'						WHERE Recency_Scale LIKE  '[4]%' AND Frequency_Scale LIKE '[1]%' 	 --
+UPDATE RFM2012 SET Segment ='New Customers'					WHERE Recency_Scale LIKE  '[5]%' AND Frequency_Scale LIKE '[1]%' 	 
+UPDATE RFM2012 SET Segment ='Potential Loyalist'			WHERE Recency_Scale LIKE  '[4-5]%' AND Frequency_Scale LIKE '[2-3]%' --
+UPDATE RFM2012 SET Segment ='Champions'						WHERE Recency_Scale LIKE  '[5]%' AND Frequency_Scale LIKE '[4-5]%' 
+GO
+UPDATE RFM2012 set Segment2= r2.segment FROM RFM2012 R1 inner join RFM_DEF R2 on r1.score=r2.score  
+GO
+ALTER INDEX [ClusteredIndex-CustomerID] ON [dbo].[RFM2012] REBUILD PARTITION = ALL WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+GO
---
+++ RFM_SEGMENT_DEF.sql
@@ -0,0 +1,193 @@
+/*
+Tahsin Bekir YAKTU 
+https://github.com/tyaktu/CRMAnalytics
+https://medium.com/@tahsinyaktu
+www.linkedin.com/in/tby1
+*/
+
+CREATE TABLE RFM_DEF(
+		[ScoreID] [int] IDENTITY(1,1) NOT NULL,
+		[SegmentID] int NULL,
+		[Segment] [varchar](50) NULL,
+		[Score] [int] NULL
+	) ON [PRIMARY]
+
+
+-- Champions
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(1, 'Champions', '555'),
+(1, 'Champions', '554'),
+(1, 'Champions', '544'),
+(1, 'Champions', '545'),
+(1, 'Champions', '454'),
+(1, 'Champions', '455'),
+(1, 'Champions', '445');
+
+-- Loyal
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(2, 'Loyal', '543'),
+(2, 'Loyal', '444'),
+(2, 'Loyal', '435'),
+(2, 'Loyal', '355'),
+(2, 'Loyal', '354'),
+(2, 'Loyal', '345'),
+(2, 'Loyal', '344'),
+(2, 'Loyal', '335');
+
+-- Potential Loyalist
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(3, 'Potential Loyalist', '553'),
+(3, 'Potential Loyalist', '551'),
+(3, 'Potential Loyalist', '552'),
+(3, 'Potential Loyalist', '541'),
+(3, 'Potential Loyalist', '542'),
+(3, 'Potential Loyalist', '533'),
+(3, 'Potential Loyalist', '532'),
+(3, 'Potential Loyalist', '531'),
+(3, 'Potential Loyalist', '452'),
+(3, 'Potential Loyalist', '451'),
+(3, 'Potential Loyalist', '442'),
+(3, 'Potential Loyalist', '441'),
+(3, 'Potential Loyalist', '431'),
+(3, 'Potential Loyalist', '453'),
+(3, 'Potential Loyalist', '433'),
+(3, 'Potential Loyalist', '432'),
+(3, 'Potential Loyalist', '423'),
+(3, 'Potential Loyalist', '353'),
+(3, 'Potential Loyalist', '352'),
+(3, 'Potential Loyalist', '351'),
+(3, 'Potential Loyalist', '342'),
+(3, 'Potential Loyalist', '341'),
+(3, 'Potential Loyalist', '333'),
+(3, 'Potential Loyalist', '323');
+
+-- New Customers
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(4, 'New Customers', '512'),
+(4, 'New Customers', '511'),
+(4, 'New Customers', '422'),
+(4, 'New Customers', '421'),
+(4, 'New Customers', '412'),
+(4, 'New Customers', '411'),
+(4, 'New Customers', '311');
+
+-- Promising
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(5, 'Promising', '525'),
+(5, 'Promising', '524'),
+(5, 'Promising', '523'),
+(5, 'Promising', '522'),
+(5, 'Promising', '521'),
+(5, 'Promising', '515'),
+(5, 'Promising', '514'),
+(5, 'Promising', '513'),
+(5, 'Promising', '425'),
+(5, 'Promising', '424'),
+(5, 'Promising', '413'),
+(5, 'Promising', '414'),
+(5, 'Promising', '415'),
+(5, 'Promising', '315'),
+(5, 'Promising', '314'),
+(5, 'Promising', '313');
+
+-- Need Attention
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(6, 'Need Attention', '535'),
+(6, 'Need Attention', '534'),
+(6, 'Need Attention', '443'),
+(6, 'Need Attention', '434'),
+(6, 'Need Attention', '343'),
+(6, 'Need Attention', '334'),
+(6, 'Need Attention', '325'),
+(6, 'Need Attention', '324');
+
+-- About To Sleep
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(7, 'About To Sleep', '331'),
+(7, 'About To Sleep', '321'),
+(7, 'About To Sleep', '312'),
+(7, 'About To Sleep', '221'),
+(7, 'About To Sleep', '213'),
+(7, 'About To Sleep', '231'),
+(7, 'About To Sleep', '241'),
+(7, 'About To Sleep', '251');
+
+-- Cannot Lose Them But Losing
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(8, 'Cannot Lose Them But Losing', '155'),
+(8, 'Cannot Lose Them But Losing', '154'),
+(8, 'Cannot Lose Them But Losing', '144'),
+(8, 'Cannot Lose Them But Losing', '214'),
+(8, 'Cannot Lose Them But Losing', '215'),
+(8, 'Cannot Lose Them But Losing', '115'),
+(8, 'Cannot Lose Them But Losing', '114'),
+(8, 'Cannot Lose Them But Losing', '113');
+
+-- At Risk
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(9, 'At Risk', '255'),
+(9, 'At Risk', '254'),
+(9, 'At Risk', '245'),
+(9, 'At Risk', '244'),
+(9, 'At Risk', '253'),
+(9, 'At Risk', '252'),
+(9, 'At Risk', '243'),
+(9, 'At Risk', '242'),
+(9, 'At Risk', '235'),
+(9, 'At Risk', '234'),
+(9, 'At Risk', '225'),
+(9, 'At Risk', '224'),
+(9, 'At Risk', '153'),
+(9, 'At Risk', '152'),
+(9, 'At Risk', '145'),
+(9, 'At Risk', '143'),
+(9, 'At Risk', '142'),
+(9, 'At Risk', '135'),
+(9, 'At Risk', '134'),
+(9, 'At Risk', '133'),
+(9, 'At Risk', '125'),
+(9, 'At Risk', '124');
+
+-- Hibernating Customers
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(10, 'Hibernating Customers', '332'),
+(10, 'Hibernating Customers', '322'),
+(10, 'Hibernating Customers', '233'),
+(10, 'Hibernating Customers', '232'),
+(10, 'Hibernating Customers', '223'),
+(10, 'Hibernating Customers', '222'),
+(10, 'Hibernating Customers', '132'),
+(10, 'Hibernating Customers', '123'),
+(10, 'Hibernating Customers', '122'),
+(10, 'Hibernating Customers', '212'),
+(10, 'Hibernating Customers', '211');
+
+-- Losing But Engaged
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(11, 'Losing But Engaged', '111'),
+(11, 'Losing But Engaged', '112'),
+(11, 'Losing But Engaged', '121'),
+(11, 'Losing But Engaged', '131'),
+(11, 'Losing But Engaged', '141'),
+(11, 'Losing But Engaged', '151');
+
+-- Lost Customers
+INSERT INTO RFM_DEF ([SegmentID] ,Segment, Score)
+VALUES
+(12, 'Lost Customers', '111'),
+(12, 'Lost Customers', '112'),
+(12, 'Lost Customers', '121'),
+(12, 'Lost Customers', '131'),
+(12, 'Lost Customers', '141'),
+(12, 'Lost Customers', '151');
